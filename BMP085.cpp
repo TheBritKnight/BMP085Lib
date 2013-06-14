dcc116a2e37e2399ce5106d1f702098e2b0e743a
TheBritKnight <charles.dwsn@gmail.com>
2013-06-13 22:59:58 -0400

    Added LICENSE.txt. Updated license statements in files

--- BMP085.cpp
+++ BMP085.cpp
@@ -9,8 +9,6 @@
 	 //just wrapped up nicely in library form
 
 /*
-Copyright (c) 2013 Charles Dawson
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
